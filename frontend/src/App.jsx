35b9e77bb1de1a9e60a41b9c28b04ed240fa5b42
Sudhir0302 <141290194+Sudhir0302@users.noreply.github.com.>
2024-10-14 19:38:40 +0530

    configured tailwindcss

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className='text-2xl text-center'>
+      <h1>Podcast</h1>
+    </div>
+  )
+}
+
+export default App
